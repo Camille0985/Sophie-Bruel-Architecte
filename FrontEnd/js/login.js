fa18019cdf2b8cd58c85376bfd02151b51ad1d86
Camille Duprez <launchmesomegames@gmail.com>
2023-02-17 11:56:00 +0100

    intégration set pour filtrer par catégorie

---
+++ FrontEnd/js/login.js
@@ -0,0 +1,3 @@
+document.querySelector('form input[type="button"]').addEventListener("click", function(){
+    for(let input of document.querySelectorAll("form input"))
+})
--- FrontEnd/js/works.js
+++ FrontEnd/js/works.js
@@ -11,32 +11,73 @@ class WorksManager{
 }
 
 
+let tabSet = [];
+let myContainer;
+let myImage;
+let myFigcaption;
+let myFigcaptionContent;
+
+
 fetch('http://localhost:5678/api/works')
     .then(data => data.json())
     .then(dataListWorks => {
         for(let dataWorks of dataListWorks){
             let works = new Works(dataWorks);
             
-            const myContainer = document.createElement("figure");
+            myContainer = document.createElement("figure");
+            myContainer.setAttribute("id", `${works.id}`);
             document.getElementsByClassName('gallery')[0].append(myContainer);
-            const myImage = document.createElement('img');
+            myImage = document.createElement('img');
             myContainer.appendChild(myImage);
             let img = myImage;
             img.src = `${works.imageUrl}`;
             img.crossOrigin = "anonymous";
-            const myFigcaption = document.createElement("figcaption");
+            myFigcaption = document.createElement("figcaption");
             myContainer.appendChild(myFigcaption);
-            const myFigcaptionContent = document.createTextNode(`${works.title}`)
+            myFigcaptionContent = document.createTextNode(`${works.title}`)
             myFigcaption.appendChild(myFigcaptionContent);
-                        
-        };        
+            if (typeof tabSet[works.categoryId] === "undefined"){
+                tabSet[works.categoryId] = new Set();
+            } 
+            tabSet[works.categoryId].add(works.id)
+            console.log(myContainer.id);
+        };
+        
+        console.log(tabSet);        
     });
+
+
+    class Categories{
+        constructor(dataCategories){
+            dataCategories && Object.assign(this, dataCategories);
+        }
+    };
+    
+    class CategoriesManager{
+        constructor(listCategories){
+            this.listCategories = listCategories;
+        }
+    }
+    let tabSet2 = [];
+    let myFilters;
 
     fetch('http://localhost:5678/api/categories')
     .then(data => data.json())
-    .then(dataListWorks => {
-        for(let dataWorks of dataListWorks){
-            let categories = new Categories(dataWorks);
-    
-        };    
-    });
+    .then(dataListCategories => {
+        const allWorks = document.createElement('button');
+        document.getElementsByClassName('filters')[0].append(allWorks);
+        const allWorksName = document.createTextNode('Tous');
+        allWorks.appendChild(allWorksName);
+        for(let dataCategories of dataListCategories){
+            let categories = new Categories(dataCategories);
+            myFilters = document.createElement('button');
+            document.getElementsByClassName('filters')[0].append(myFilters);
+            const myFiltersName = document.createTextNode(categories.name);
+            myFilters.appendChild(myFiltersName);
+
+        };
+
+
+
+    })
+
