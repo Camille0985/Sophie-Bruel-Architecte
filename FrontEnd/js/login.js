912252266fdb2f156e57e39b29a1ff4ee3991bce
Camille Duprez <launchmesomegames@gmail.com>
2023-03-08 09:50:10 +0100

    Page connection, récupération et stockage du token, début du site en mode admin

--- FrontEnd/js/login.js
+++ FrontEnd/js/login.js
@@ -1,3 +1,49 @@
-document.querySelector('form input[type="button"]').addEventListener("click", function(){
-    for(let input of document.querySelectorAll("form input"))
-})
+/*document.querySelector('form input[type="button"]').addEventListener("click", function(){
+    let valid = true;
+    for(let input of document.querySelectorAll("form input, form text")) {
+        valid = valid && input.reportValidity();
+        if(!valid) {
+            break;
+          }
+        }
+        if(valid) {
+          alert('Connexion');
+        }
+});*/
+let btnConnect = document.getElementById('login-btn-connect');
+btnConnect.addEventListener("click", connectUser);
+
+
+  function connectUser() {
+    localStorage.setItem('token', '');
+    let user = {
+      email: document.getElementById('login-email').value,
+      password: document.getElementById('login-password').value
+    };
+      
+      fetch('http://localhost:5678/api/users/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(user)
+      })
+      .then(function(response){
+        return response.json()
+      })
+    
+      .then(function(data) {
+        console.log(data);
+        if (typeof data.token !== 'undefined') {
+        localStorage.setItem('userId', data.userId);
+        localStorage.setItem('token', data.token);
+        if (localStorage.getItem('token') !== '') {
+          window.location.href = "index.html";
+        } else {
+          alert('not ok');
+        }    
+      } else {
+        alert('identifiants incorrects')
+      }
+      });
+  };
